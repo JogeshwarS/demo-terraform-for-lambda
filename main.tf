49f13203ba04cad52e9edb899b90f289cc19df26
shubham <shubham@gmail.com>
2023-10-26 15:16:56 +0530

    files updated

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-west-1"
+  profile = "myprofile"  
+}
+
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "my-lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = var.function_name
+  handler      = "index.handler"
+  runtime      = "python3.11"
+  role        = aws_iam_role.lambda_execution_role.arn
+  filename     = "/home/xubham/Desktop/terraform/stage-tag-script-peer.zip"  
+
+  timeout      = 10
+  memory_size = 128
+}
---
+++ var.tf
@@ -0,0 +1,6 @@
+# Define the variable for the function name
+variable "function_name" {
+  type    = string
+  default = "my-lambda-function"
+}
+
